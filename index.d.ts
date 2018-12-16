ed7ad40808d8e317c9830cafc6cc2262bb46bf58
spacejack <spacejack@gmail.com>
2018-12-15 20:52:34 -0500

    Various type fixes

--- index.d.ts
+++ index.d.ts
@@ -5,33 +5,35 @@
 // TypeScript Version: 3.0
 
 declare namespace Ospec {
-	type Assertion = (value: any) => (description: string) => void;
+	type Assertion = (description: string) => void;
 
-	interface Spy {
-		(...args: any[]): void;
+	interface Spy<A extends any[], R> {
+		(...args: A): R;
 		callCount: number;
-		args: any[];
+		args: A[];
 	}
 
 	interface Test {
-		equals: Assertion;
-		notEquals: Assertion;
-		deepEquals: Assertion;
-		notDeepEquals: Assertion;
+		equals(other: any): Assertion;
+		notEquals(other: any): Assertion;
+		deepEquals(other: any): Assertion;
+		notDeepEquals(other: any): Assertion;
 	}
 
 	interface Static {
 		(value: any): Test;
-		(name: string, f: (done: () => void) => void): void;
-		spec(name: string, f: () => void): void;
-		before(f: () => void): void;
-		beforeEach(f: () => void): void;
-		after(f: () => void): void;
-		afterEach(f: () => void): void;
-		spy(f?: (...args: any[]) => any): Spy;
+		(name: string, fn: () => void | Promise<void>): void;
+		(name: string, fn: (done: (err?: Error) => void, timeout: (delay: number) => void) => void): void;
+		spec(name: string, fn: () => void): void;
+		before(fn: () => void): void;
+		beforeEach(fn: () => void): void;
+		after(fn: () => void): void;
+		afterEach(fn: () => void): void;
+		spy<A extends any[] = any[]>(): Spy<A, undefined>;
+		spy<A extends any[], R>(fn: (...args: A) => R): Spy<A, R>;
 		timeout(delay: number): void;
 		run(): void;
-		new: () => Static;
+		"new"(): Static;
 	}
 }
 
--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,7 @@
 import * as o from '..';
 
 o.spec('types', () => {
-    o(1).equals(1)
-})
+    o(1).equals(1);
+});
 
 // TODO: Tests...
